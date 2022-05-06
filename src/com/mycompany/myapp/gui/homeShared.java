087d903fbfd890bdabf6e923f11940eb6aebf233
iheb2306 <IhebKraiem>
2022-05-07 00:15:59 +0100

    Initial commit

---
+++ src/com/mycompany/myapp/gui/homeShared.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myapp.gui;
+
+import com.codename1.ui.Button;
+import com.codename1.ui.Component;
+import com.codename1.ui.Form;
+import com.codename1.ui.Label;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+
+import static com.codename1.ui.CN.*;
+import com.codename1.ui.Form;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.plaf.UIManager;
+import com.codename1.ui.util.Resources;
+import com.codename1.io.Log;
+import com.codename1.ui.Toolbar;
+import com.mycompany.myapp.gui.GetOffres;
+
+/**
+ *
+ * @author user
+ */
+public class homeShared extends Form{
+    Form current;
+    private Resources theme;
+      public homeShared() {
+         
+        setTitle("Liste des Destinations");
+        setScrollableY(true);
+}
+}
